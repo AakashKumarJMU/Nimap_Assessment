6c4d3a9307e5d88754f1ae1dfd8a87eb1a0f9561
Aakash Kumar <161213128+AakashKumarJMU@users.noreply.github.com>
2024-11-30 15:36:24 +0530

    Create NimapProductApplication.java

---
+++ src/main/java/com/Aakash/nimap/NimapProductApplication.java
@@ -0,0 +1,14 @@
+
+package com.aakash.nimap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimapProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimapProductApplication.class, args);
+	}
+
+}
